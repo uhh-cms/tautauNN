634f6954274a9511483f5384863ac7d4d71d3d3e
Johan Wulff <jwulff@lip.pt>
2024-02-12 17:36:52 +0100

    add new baseline

--- tautaunn/config.py
+++ tautaunn/config.py
@@ -80,6 +80,13 @@ btag_wps = {
     },
 }
 
+metnomu_et_cuts = {
+    "2016APV": 160,
+    "2016": 160, 
+    "2017": 180, 
+    "2018": 180, 
+}
+
 masses = [
     250, 260, 270, 280, 300, 320, 350, 400, 450, 500, 550, 600, 650,
     700, 750, 800, 850, 900, 1000, 1250, 1500, 1750, 2000, 2500, 3000,
@@ -479,6 +486,17 @@ selection_sets = {
         ]
         for year, w in btag_wps.items()
     },
+    "new_skims_baseline":{
+        year: ["nleps == 0",
+               "(nbjetscand > 1 | isBoosted == 1)",
+               (
+               f"((isLeptrigger == 1) | (isMETtriggerNoThresh & metnomu_et > {m[year]})) & (pairType == 0)) | ",
+               f"((isLeptrigger == 1) | (isMETtriggerNoThresh & metnomu_et > {m[year]})) & (pairType == 1)) | ",
+               f"((isLeptrigger == 1) | (isMETtriggerNoThresh & metnomu_et > {m[year]}) | isSingleTauTrigger ) & (pairType == 2))",
+               )
+               ]
+               for year, m in metnomu_et_cuts.items()
+    }
 }
 
 klub_aliases: dict[str, str] = {
