cc73c5543f53cc85af78c122dc9a333c82df2902
Johan Wulff <jwulff@lip.pt>
2024-06-28 21:19:42 +0200

    update plots

--- tautaunn/plot_dists.py
+++ tautaunn/plot_dists.py
@@ -31,6 +31,8 @@ def make_parser():
     parser.add_argument("-o", "--output_dir",
                         type=str, help='output dir to store plots (default ./input_dir.stem)',
                         default="")
+    parser.add_argument("-y", "--year", required=True, type=str, help='2016, 2016APV, 2017 or 2018')
+    parser.add_argument("-s", "--spin", required=False, default=None, type=str, help="spin of the signal sample")
     parser.add_argument("-l", "--limits_file", required=False, default=None,
                         type=str, help='/full/path/to/reslimits.npz file')
     return parser
@@ -148,6 +150,113 @@ def plot_hist_cms_style(bkgd_stack: hist.Stack,
     plt.close()
 
 
+def plot_mc_stat(mc: Stack,
+                 ax: plt.Axes,
+                 mode: str = "errorbar",):
+    assert mode in ("errorbar", "ratio")
+
+    mc_stat = Hist.new.Var(sum(mc).axes[0].edges, name="mc_stat").Weight()
+    mc_stat.view().value = np.sqrt(sum(mc).variances())
+    if mode == "errorbar":
+        bottom = sum(mc).values()
+        mc_stat.view().value = np.sqrt(sum(mc).variances())
+    elif mode == "ratio":
+        bottom = 1.
+        mc_stat.view().value = np.nan_to_num(np.divide(np.sqrt(sum(mc).variances()), sum(mc).values()), nan=0.)
+    for sign in (-1., 1.):
+        ax.hist([mc_stat.axes[0].edges[:-1]], bins=mc_stat.axes[0].edges, weights=sign*mc_stat.values(),
+                        bottom=bottom, histtype="stepfilled", color="gainsboro",
+                        hatch='///', edgecolor="gray", linewidth=0., alpha=0.6)
+
+# put the above into a function
+def plot_mc_data_sig(data_hist: Hist,
+                     signal_hist: Hist,
+                     bkgd_stack: Stack,
+                     bin_edges: list,
+                     year: str,
+                     channel: str,
+                     cat: str,
+                     savename: str | Path,
+                     signal_name: str,
+                     sb_limit: float = 0.05, 
+                     limit_value = None,
+                     ) -> None:
+
+    if limit_value is None:
+        limit_value = 1
+
+    signal_hist *= limit_value
+    mask = (signal_hist.values()/ sum(bkgd_stack).values()) < sb_limit 
+    # blind data
+    data_hist.values()[~mask] = np.nan
+    data_hist.variances()[~mask] = np.nan
+    
+    color_map = {
+        "DY": "#7a21dd",
+        "TT": "#9c9ca1",
+        "ST": "#e42536",
+        "W": "#964a8b",
+        "QCD": "#f89c20",
+        "Others":"#5790fc",
+    }
+    lumi = {"2016APV": "19.5", "2016": "16.8", "2017": "41.5", "2018": "59.7"}[year]
+
+    fig, (ax1, ax2) = plt.subplots(2, 1,
+                                figsize=(10, 12),
+                                sharex=True,
+                                gridspec_kw={'height_ratios': [4, 1]},)
+    fig.subplots_adjust(hspace=0.05)
+    hep.cms.text(" Preliminary", fontsize=20, ax=ax1)
+    mu, tau = '\u03BC','\u03C4'
+    chn_map = {"etau": r"$bbe$"+tau, "tautau":r"$bb$"+tau+tau, "mutau": r"$bb$"+mu+tau}
+    hep.cms.lumitext(r"{} $fb^{{-1}}$ (13 TeV)".format(lumi), fontsize=20, ax = ax1)
+    ax1.text(0.05, .91, f"{chn_map[channel]}\n{cat}", fontsize=15,transform=ax1.transAxes)
+    
+    bkgd_stack.plot(stack=True, ax=ax1, color=[color_map[i.name] for i in bkgd_stack], histtype='fill')
+    plot_mc_stat(bkgd_stack, ax1, mode="errorbar")
+    data_hist.plot(color='black', ax=ax1, label="data", histtype='errorbar')
+    signal_hist.plot(color='black', ax=ax1, label=f"{signal_name} scaled to\nexp. limit: {limit_value:.1f} pb",)
+    
+    if any(mask):
+        idx = np.where(mask)[0][-1] + 1
+        x = signal_hist.axes[0].edges[idx]
+        y = sum(bkgd_stack).values()[idx-1]*2 
+        ax1.vlines(x, 0, y,
+                color='red', linestyle='--', label=f"S/B {sb_limit:.0%} edge")
+        
+    lgd = ax1.legend( fontsize = 12,bbox_to_anchor = (0.99, 0.99), loc="upper right", ncols=2,
+                    frameon=True, facecolor='white', edgecolor='black')
+    lgd.get_frame().set_boxstyle("Square", pad=0.0)
+    ax1.set_yscale("log")
+    min_y_tt_dy = min([bkgd_stack[h].values().min() for h in ["DY", "TT"]])
+    min_y_sig = signal_hist.values().min()
+    min_y = min(min_y_tt_dy, min_y_sig)
+    max_y = sum(bkgd_stack).values().max()
+    ax1.set_ylim((min_y, 10 * max_y))
+    ax1.set_xlabel("")
+    ax1.set_ylabel("Events")
+    
+    ax2.set_xlabel("pDNN Score")
+    ax2.set_ylabel("Data/MC")
+    ratio_hist = Hist.new.Var(data_hist.axes[0].edges, name="ratio").Weight()
+    ratio_hist.view().value = data_hist.values()/sum(bkgd_stack).values()
+    ratio_hist.view().variance = np.divide(np.sqrt(data_hist.variances()), data_hist.values())
+
+    ratio_hist.view().value[~mask] = np.nan
+    ratio_hist.variances()[~mask] = np.nan
+
+    ax2.hlines(1, 0, 1, color='black', linestyle='--')
+    ax2.hlines([0.5, 1.5], 0, 1, color='grey', linestyle='--')
+    ax2.errorbar(ratio_hist.axes[0].centers, ratio_hist.values(), yerr=ratio_hist.variances(), fmt='o', color='black')
+    plot_mc_stat(bkgd_stack, ax2, mode="ratio")
+    ax2.set_ylim(0.4, 1.6)
+    ax2.set_xlim(0, 1)
+    ax2.set_xticks(signal_hist.axes[0].edges, [round(i, 4) for i in bin_edges], rotation=60)
+    if not Path(savename).parent.exists():
+        os.makedirs(Path(savename).parent)
+    plt.savefig(savename, bbox_inches='tight', pad_inches=0.05)
+    plt.close()
+    
 def plot_partially_unblinded(bkgd_stack: hist.Stack,
                         signal_hist: hist,
                         data_hist: hist,
@@ -183,8 +292,7 @@ def plot_partially_unblinded(bkgd_stack: hist.Stack,
     mu, tau = '\u03BC','\u03C4'
     chn_map = {"etau": r"$bbe$"+tau, "tautau":r"$bb$"+tau+tau, "mutau": r"$bb$"+mu+tau}
     hep.cms.lumitext(r"{} $fb^{{-1}}$ (13 TeV)".format(lumi), fontsize=20, ax = ax1)
-    ax1.text(0.32, 1.013, chn_map[channel], fontsize=13,transform=ax1.transAxes)
-    ax1.text(0.39, 1.013, cat, fontsize=13, transform=ax1.transAxes)
+    ax1.text(0.05, .91, f"{chn_map[channel]}\n{cat}", fontsize=15,transform=ax1.transAxes)
     bkgd_stack.plot(stack=True, ax=ax1, color=[color_map[i.name] for i in bkgd_stack], histtype='fill')
     # scale signal to the limit (must be given in pb) 
     signal_hist *= limit_value
@@ -212,7 +320,7 @@ def plot_partially_unblinded(bkgd_stack: hist.Stack,
     min_y_sig = signal_hist.values().min()
     min_y = min(min_y_tt_dy, min_y_sig)
     max_y = summed_hist.values().max() 
-    ax1.set_ylim((min_y, 2 * max_y))
+    ax1.set_ylim((min_y, 10 * max_y))
 
     ax1.set_xlabel("")
     ax1.set_ylabel("Events")
@@ -238,6 +346,8 @@ def plot_partially_unblinded(bkgd_stack: hist.Stack,
 
 def make_plots(input_dir: str | Path,
                output_dir: str | Path,
+               year: str,
+               spin: str,
                limits_file: str | Path | None = None):
     if output_dir == "":
         output_dir = f"./{Path(input_dir).parent.stem}"
@@ -247,27 +357,29 @@ def make_plots(input_dir: str | Path,
             os.makedirs(output_dir)
         else:
             raise ValueError(f"Output dir {output_dir} does not exist and wasn't created")
-    datashapes = glob(f'{input_dir}/shapes_*.root')
+    datashapes = glob(f'{input_dir}/shapes_cat_{year}_*_spin_{spin}_*.root')
     datashapes = [shape for shape in datashapes
                   if not any(s in shape for s in ['mwc', 'mdnn', 'mhh'])]
     for file in tqdm(datashapes):
         filename = Path(file)
-        _, _, year, channel, cat, sign, isolation, _, spin, _, mass = filename.stem.split("_")
+        _, _, _, channel, cat, sign, isolation, _, spin, _, mass = filename.stem.split("_")
         dirname = f"cat_{year}_{channel}_{cat}_{sign}_{isolation}"
         signal_name = f"ggf_spin_{spin}_mass_{mass}_{year}_hbbhtt"
         stack, sig, data, bin_edges = load_hists(filename, dirname, signal_name, year)
         if limits_file is not None:
             lim = load_reslim(limits_file, mass)
-            plot_partially_unblinded(bkgd_stack=stack,
-                                    signal_hist=sig,
-                                    data_hist=data,
-                                    bin_edges=bin_edges,
-                                    year=year,
-                                    channel=channel,
-                                    signal_name=" ".join(signal_name.split("_")[0:5]).replace("ggf", "ggf;").replace("spin ", 's:').replace("mass ", "m:"),
-                                    cat=cat,
-                                    savename=f"{output_dir}/{year}/{channel}/{cat}/{filename.stem}.png",
-                                    limit_value=lim)
+            signal_name = " ".join(signal_name.split("_")[0:5]).replace("ggf", "ggf;").replace("spin ", 's:').replace("mass ", "m:")
+            plot_mc_data_sig(data_hist=data,
+                             signal_hist=sig,
+                             bkgd_stack=stack,
+                             bin_edges=bin_edges,
+                             year=year,
+                             channel=channel,
+                             cat=cat,
+                             signal_name=signal_name,
+                             savename=f"{output_dir}/{year}/{channel}/{cat}/{filename.stem}.png",
+                             sb_limit=0.05,
+                             limit_value=lim)
         else: 
             lim = None
             plot_hist_cms_style(bkgd_stack=stack,
@@ -282,9 +394,13 @@ def make_plots(input_dir: str | Path,
 
 def main(input_dir: str | Path,
          output_dir: str | Path,
+         year: str,
+         spin: str,
          limits_file: str | Path | None) -> None:
     make_plots(input_dir=input_dir,
                output_dir=output_dir,
+               year=year,
+               spin=spin,
                limits_file=limits_file)
 
 
@@ -293,6 +409,8 @@ if __name__ == "__main__":
     args = parser.parse_args()
     main(input_dir=args.input_dir,
          output_dir=args.output_dir,
+         year=args.year,
+         spin=args.spin,
          limits_file=args.limits_file)
 
 
