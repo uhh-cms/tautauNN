f4da36caeedc709e684774ad32a7ab7bb4040cea
Johan Wulff <jwulff@lip.pt>
2025-01-20 20:37:44 +0100

    remove year parametrization for control_region

--- tautaunn/control_region.py
+++ tautaunn/control_region.py
@@ -42,8 +42,9 @@ def write_datacards(
     spin: int | Sequence[int],
     mass: int | Sequence[int],
     category: str | Sequence[str],
-    skim_directories: dict[str, str],
-    eval_directories: dict[str, str],
+    sample_names: list[str],
+    skim_directory: list[str],
+    eval_directory: list[str],
     output_directory: str,
     n_bins: int = 5,
     output_pattern: str = "cat_{category}_spin_{spin}_mass_{mass}",
@@ -70,17 +71,7 @@ def write_datacards(
         assert mass in masses
     for category in _categories:
         assert category in categories
-
-
-    # check that all skim directories are valid
-    sample_names = []
-    for sample_name, skim_dir in skim_directories:
-        if not os.path.isdir(skim_dir):
-            raise Exception(f"'{skim_dir}' is not a valid skim directory")
-        sample_names.append(skim_dir.split("/")[-1])
         
-    assert "_".join(sample_names) == "_".join([s.split("/")[-1] for s in eval_directories])
-
     # get a mapping of process name to sample names
     sample_map: dict[str, list] = {} 
     all_matched_sample_names: list[str] = [] 
@@ -93,18 +84,18 @@ def write_datacards(
             continue
 
         # match sample names
+        matched_sample_names = []
         for sample_name in sample_names:
-            matched_sample_names = []
             if any(fnmatch(sample_name, pattern) for pattern in process_data["sample_patterns"]):
                 if sample_name in matched_sample_names:
                     raise Exception(f"sample '{sample_name}' already matched by a previous process")
                 all_matched_sample_names.append(sample_name)
                 matched_sample_names.append(sample_name)
                 continue
-            if not matched_sample_names:
-                print(f"process '{process_name}' has no matched samples, skipping")
-                continue
-            sample_map[process_name] = matched_sample_names
+        if not matched_sample_names:
+            print(f"process '{process_name}' has no matched samples, skipping")
+            continue
+        sample_map[process_name] = matched_sample_names
 
     # ensure that the output directory exists
     output_directory = os.path.expandvars(os.path.expanduser(output_directory))
@@ -121,8 +112,8 @@ def write_datacards(
     print(f"going to load {len(all_matched_sample_names)} samples")
     sample_data = {
             sample_name: load_sample_data(
-                skim_directories[i],
-                eval_directories[i],
+                skim_directory,
+                eval_directory,
                 year,
                 sample_name,
                 dnn_output_columns,
@@ -183,12 +174,9 @@ def _write_datacard(
 
     # check if there is data provided for this category if it is bound to a year
     assert cat_data["year"] in list(luminosities.keys()) + [None]
-    if cat_data["year"] is not None and not any(cat_data["year"] == year for year in sample_data):
-        print(f"category {category} is bound to a year but no data was provided for that year")
-        return (None, None)
 
-
-    datacard_year = datacard_years[cat_data["year"]]
+    year = cat_data["year"]
+    datacard_year = datacard_years[year]
 
     # check if the requested category is indeed a control region
     if not "_cr" in category:
@@ -270,7 +258,7 @@ def _write_datacard(
     # apply the category selection to sample data
     sample_data = {
             sample_name: sample_data[sample_name][cat_data["selection"](sample_data[sample_name], year=year)]
-            for sample_name, process_name in sample_processes[year].items()
+            for sample_name, process_name in sample_processes.items()
     }
 
     # complain when nan's were found
@@ -296,7 +284,7 @@ def _write_datacard(
     # transpose the sample_map so that we have a "process -> year -> sample_names" mapping
     process_map = defaultdict(dict)
     for process_name, sample_names in sample_map.items():
-        process_map[process_name][year] = sample_names
+        process_map[process_name] = sample_names
 
     # histogram structures
     # mapping process -> (nuisance, direction) -> hist
@@ -358,7 +346,7 @@ def _write_datacard(
                     if not is_data:
                         weight = sample_data[sample_name][varied_weight_field] * scale
                     h.fill(**{
-                        full_hist_name: sample_data[year][sample_name][varied_variable_name],
+                        full_hist_name: sample_data[sample_name][varied_variable_name],
                         "weight": weight,
                     })
 
@@ -371,109 +359,105 @@ def _write_datacard(
             # actual qcd estimation
             if qcd_estimation:
                 # mapping year -> region -> hist
-                qcd_hists: dict[str, dict[str, tuple[hist.Hist, hist.Hist]]] = defaultdict(dict)
-
+                qcd_hists: dict[str, tuple[hist.Hist, hist.Hist]] = defaultdict(dict)
                 # create data-minus-background histograms in the 4 regions
                 for region_name, _qcd_data in qcd_data.items():
-                    for year, data in _qcd_data.items():
-                        datacard_year = datacard_years[year]
-                        # create a histogram that is filled with both data and negative background
-                        full_hist_name = ShapeNuisance.create_full_name(hist_name, year=datacard_year)
-                        h_data = hist.Hist.new.Variable(bin_edges, name=f"{full_hist_name}_data").Weight()
-                        h_mc = hist.Hist.new.Variable(bin_edges, name=f"{full_hist_name}_mc").Weight()
-                        for sample_name, _data in data.items():
-                            process_name = sample_processes[year][sample_name]
-                            if not nuisance.applies_to_process(process_name):
-                                continue
-
-                            # skip signals
-                            if processes[process_name].get("signal", False):
-                                continue
-
-                            is_data = processes[process_name].get("data", False)
-                            if is_data:
-                                # for data, always will the nominal values
-                                h_data.fill(**{
-                                    f"{full_hist_name}_data": _data[variable_name],
-                                    "weight": 1,
-                                })
-                            else:
-                                scale = luminosities[year]
-                                h_mc.fill(**{
-                                    f"{full_hist_name}_mc": _data[varied_variable_name],
-                                    "weight": _data[varied_weight_field] * scale,
-                                })
-                        # subtract the mc from the data
-                        # h_qcd = hist.Hist.new.Variable(bin_edges, name=f"{full_hist_name}").Weight()
-                        # h_qcd.view().value[...] = h_data.view().value - h_mc.view().value
-                        # h_qcd.view().variance[...] = h_mc.view().variance
-                        # qcd_hists[year][region_name] = h_qcd
-                        qcd_hists[year][region_name] = (h_data, h_mc)
-
-                # ABCD method per year
-                # TODO: consider using averaging between the two options where the shape is coming from
-                for year, region_hists in qcd_hists.items():
                     datacard_year = datacard_years[year]
+                    # create a histogram that is filled with both data and negative background
                     full_hist_name = ShapeNuisance.create_full_name(hist_name, year=datacard_year)
-                    h_qcd = hist.Hist.new.Variable(bin_edges, name=f"{full_hist_name}").Weight()
-                    # shape placeholders
-                    B, C, D = "ss_iso", "os_noniso", "ss_noniso"
-                    # test
-                    # B, C, D = "os_noniso", "ss_iso", "ss_noniso"
-                    h_data_b, h_mc_b = region_hists[B]
-                    h_data_c, h_mc_c = region_hists[C]
-                    h_data_d, h_mc_d = region_hists[D]
-                    # compute transfer factor and separate mc and data uncertainties
-                    int_data_c = Number(h_data_c.sum().value, {"data": h_data_c.sum().variance**0.5})
-                    int_data_d = Number(h_data_d.sum().value, {"data": h_data_d.sum().variance**0.5})
-                    int_mc_c = Number(h_mc_c.sum().value, {"mc": h_mc_c.sum().variance**0.5})
-                    int_mc_d = Number(h_mc_d.sum().value, {"mc": h_mc_d.sum().variance**0.5})
-                    # deem the qcd estimation invalid if either difference is negative
-                    qcd_invalid = (int_mc_c > int_data_c) or (int_mc_d > int_data_d)
-                    if not qcd_invalid:
-                        # compute the QCD shape with error propagation
-                        values_data_b = Number(h_data_b.view().value, {"data": h_data_b.view().variance**0.5})
-                        values_mc_b = Number(h_mc_b.view().value, {"mc": h_mc_b.view().variance**0.5})
-                        tf = (int_data_c - int_mc_c) / (int_data_d - int_mc_d)
-                        qcd = (values_data_b - values_mc_b) * tf
-                        # inject values
-                        h_qcd.view().value[...] = qcd.n
-                        # inject variances, combining data and mc uncertainties, assuming symmetric errors
-                        h_qcd.view().variance[...] = qcd.get("up", unc=True)**2
-                    # zero-fill
-                    hval = h_qcd.view().value
-                    hvar = h_qcd.view().variance
-                    zero_mask = hval <= 0
-                    # keep the variance proportion that reaches into positive values
-                    hvar[zero_mask] = (np.maximum(0, hvar[zero_mask]**0.5 + hval[zero_mask]))**2
-                    hval[zero_mask] = 1.0e-5
-                    # store it
-                    hists[(year, "QCD")][(nuisance.get_combine_name(year=datacard_year), direction)] = h_qcd
+                    h_data = hist.Hist.new.Variable(bin_edges, name=f"{full_hist_name}_data").Weight()
+                    h_mc = hist.Hist.new.Variable(bin_edges, name=f"{full_hist_name}_mc").Weight()
+                    for sample_name, _data in _qcd_data.items():
+                        process_name = sample_processes[sample_name]
+                        if not nuisance.applies_to_process(process_name):
+                            continue
 
-    # drop qcd shapes in years where no valid estimation was found
-    if qcd_estimation:
-        for year, qcd_valid in any_qcd_valid.items():
-            if not qcd_valid:
-                print(
-                    f"  completely dropping QCD shape in ({category},{year},{spin},{mass}) as no valid shape could be "
-                    "created for any nuisance",
-                )
-                del hists[(year, "QCD")]
+                        # skip signals
+                        if processes[process_name].get("signal", False):
+                            continue
+
+                        is_data = processes[process_name].get("data", False)
+                        if is_data:
+                            # for data, always will the nominal values
+                            h_data.fill(**{
+                                f"{full_hist_name}_data": _data[variable_name],
+                                "weight": 1,
+                            })
+                        else:
+                            scale = luminosities[year]
+                            h_mc.fill(**{
+                                f"{full_hist_name}_mc": _data[varied_variable_name],
+                                "weight": _data[varied_weight_field] * scale,
+                            })
+                    # subtract the mc from the data
+                    # h_qcd = hist.Hist.new.Variable(bin_edges, name=f"{full_hist_name}").Weight()
+                    # h_qcd.view().value[...] = h_data.view().value - h_mc.view().value
+                    # h_qcd.view().variance[...] = h_mc.view().variance
+                    # qcd_hists[year][region_name] = h_qcd
+                    qcd_hists[region_name] = (h_data, h_mc)
+
+            # ABCD method per year
+            # TODO: consider using averaging between the two options where the shape is coming from
+            for region_name, region_hists in qcd_hists.items():
+                datacard_year = datacard_years[year]
+                full_hist_name = ShapeNuisance.create_full_name(hist_name, year=datacard_year)
+                h_qcd = hist.Hist.new.Variable(bin_edges, name=f"{full_hist_name}").Weight()
+                # shape placeholders
+                B, C, D = "ss_iso", "os_noniso", "ss_noniso"
+                # test
+                # B, C, D = "os_noniso", "ss_iso", "ss_noniso"
+                h_data_b, h_mc_b = region_hists[B]
+                h_data_c, h_mc_c = region_hists[C]
+                h_data_d, h_mc_d = region_hists[D]
+                # compute transfer factor and separate mc and data uncertainties
+                int_data_c = Number(h_data_c.sum().value, {"data": h_data_c.sum().variance**0.5})
+                int_data_d = Number(h_data_d.sum().value, {"data": h_data_d.sum().variance**0.5})
+                int_mc_c = Number(h_mc_c.sum().value, {"mc": h_mc_c.sum().variance**0.5})
+                int_mc_d = Number(h_mc_d.sum().value, {"mc": h_mc_d.sum().variance**0.5})
+                # deem the qcd estimation invalid if either difference is negative
+                qcd_invalid = (int_mc_c > int_data_c) or (int_mc_d > int_data_d)
+                if not qcd_invalid:
+                    # compute the QCD shape with error propagation
+                    values_data_b = Number(h_data_b.view().value, {"data": h_data_b.view().variance**0.5})
+                    values_mc_b = Number(h_mc_b.view().value, {"mc": h_mc_b.view().variance**0.5})
+                    tf = (int_data_c - int_mc_c) / (int_data_d - int_mc_d)
+                    qcd = (values_data_b - values_mc_b) * tf
+                    # inject values
+                    h_qcd.view().value[...] = qcd.n
+                    # inject variances, combining data and mc uncertainties, assuming symmetric errors
+                    h_qcd.view().variance[...] = qcd.get("up", unc=True)**2
+                # zero-fill
+                hval = h_qcd.view().value
+                hvar = h_qcd.view().variance
+                zero_mask = hval <= 0
+                # keep the variance proportion that reaches into positive values
+                hvar[zero_mask] = (np.maximum(0, hvar[zero_mask]**0.5 + hval[zero_mask]))**2
+                hval[zero_mask] = 1.0e-5
+                # drop qcd shapes in years where no valid estimation was found
+                if qcd_invalid: 
+                    print(
+                        f"  completely dropping QCD shape in ({category},{year},{spin},{mass}) as no valid shape could be "
+                        "created for any nuisance",
+                    )
+                else:
+                    # store it
+                    hists["QCD"][(nuisance.get_combine_name(year=datacard_year), direction)] = h_qcd
+
 
     # gather rates from nominal histograms
     rates = {
-        (year, process_name): _hists[("nominal", "")].sum().value
-        for (year, process_name), _hists in hists.items()
+        process_name: _hists[("nominal", "")].sum().value
+        for process_name, _hists in hists.items()
     }
 
     # create process names joining raw names and years
     full_process_names = {
-        (year, process_name): (
+        process_name: (
             "{1}_{0}{2}".format(year, *m.groups())
             if (m := re.match(r"^(.+)(_h[^_]+)$", process_name))
             else f"{process_name}_{year}"
         )
-        for year, process_name in hists
+        for process_name in hists
         if process_name != "data_obs"
     }
 
@@ -482,7 +466,7 @@ def _write_datacard(
     def write(path):
         # create a dictionary of all histograms
         content = {}
-        for (year, process_name), _hists in hists.items():
+        for  process_name, _hists in hists.items():
             for (full_nuisance_name, direction), h in _hists.items():
                 # determine the full process name and optionally skip data for nuisances
                 if process_name == "data_obs":
@@ -490,7 +474,7 @@ def _write_datacard(
                         continue
                     full_process_name = process_name
                 else:
-                    full_process_name = full_process_names[(year, process_name)]
+                    full_process_name = full_process_names[process_name]
 
                 if full_nuisance_name == "nominal":
                     shape_name = shape_patterns["nom"].format(category=category, process=full_process_name)
